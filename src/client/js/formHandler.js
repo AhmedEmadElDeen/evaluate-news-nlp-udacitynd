d991b12e1e0bfc93f0a38be989014ef5c29c2953
Ahmed Emad ElDeen <64612594+AhmedEmadElDeen@users.noreply.github.com>
2021-08-30 22:27:54 +0200

    few fixes

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,37 +1,34 @@
 const handleSubmit = async (event) => {
     event.preventDefault()
 
-    let inputText = document.getElementById('url').value
+    let url = document.getElementById('url').value
 
-    if (Client.validURL(inputText)) {
-        console.log('Form Submitted')
-
-        postData('http://localhost:8080/api', { url: inputText })
-
-    } else {
-        alert('Invalid URL, please try again with a valid URL.')
-    }
-}
-
-const postData = async (url='', data={}) => {
-    console.log('Analyzing URL:', data)
-    await fetch(url, {
+    if (Client.validURL(url)) {
+        console.log(url)
+        fetch('http://localhost:8080/article-data', {
         method: 'POST',
         credentials: 'same-origin',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
-    }).then( res =>  res.json())
+        body: JSON.stringify({url})}).then( res =>  res.json())
         .then( res => {
-        console.log('Data received:', res)
+        updateResult(res)
+        })
+
+    } else {
+        alert("Sorry, I couldn't analyze the link you requested.")
+    }
+}
+
+function updateResult (res) {
+        console.log('API sentiment data:', res)
         document.getElementById('agreement').innerHTML = `Agreement: ${res.agreement}`
         document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`
         document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`
         document.getElementById('irony').innerHTML = `Irony: ${res.irony}`
         document.getElementById('score-tag').innerHTML = `Score Tag: ${res.score_tag}`
-        })
 }
 
 export { handleSubmit }
