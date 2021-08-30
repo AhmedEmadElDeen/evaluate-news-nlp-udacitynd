dcee435b8223c56cf392a307584e933d29fcec41
Ahmed Emad ElDeen <64612594+AhmedEmadElDeen@users.noreply.github.com>
2021-08-30 22:28:42 +0200

    few fixes

--- src/server/index.js
+++ src/server/index.js
@@ -7,14 +7,13 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch');
 const mockAPIResponse = require('./mockAPI.js')
+const apiKey = process.env.API_KEY
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('dist'))
-console.log(__dirname)
-
 
 // Server Setup
 const PORT = 8080
@@ -23,28 +22,25 @@ app.listen(PORT, (error) => {
     console.log(`Server listening on port ${PORT}!`)
 })
 
-
 // GET Route
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// API Setup
-const baseAPIURL = 'https://api.meaningcloud.com/sentiment-2.1?'
-const apiKey = process.env.API_KEY
-console.log(`Your API key is ${process.env.API_KEY}`)
-let userInput = []
+
 
 // POST Route
-app.post('/api', async function(req, res) {
-    userInput= req.body.url
-    console.log(`You entered: ${userInput}`)
-    const apiURL = `${baseAPIURL}key=${apiKey}&url=${userInput}&lang=en`
-
-    const response = await fetch(apiURL)
-    const data = await response.json()
-    res.send(data)
+app.post('/article-data', async function(req, res) {
+    let enteredURL = req.body.url
+    console.log(`Link: ${enteredURL}`)
+    const apiData = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&url=${enteredURL}&lang=en`)
+    try {
+        const incomingData = await apiData.json()
+        res.send(incomingData)
+    } catch(error) {
+        console.log('error', error)
+    }
 })
 
 app.get('/test', function (req, res) {
